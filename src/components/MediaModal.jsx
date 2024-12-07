aafeb409938381cfc57a671ee11239c62cc9913c
Seongnam-si <hongdev26@gmail.com>
2024-12-07 20:59:23 +0900

    feat : 정적 화면 구현 진행중 로그인 후 메인화면 미디어 재생영역 구현 진행중 1. 미디어 재생영역 관리를 위한 미디어모달 컴포넌트 생성완료 2. 미디어모달 컴포넌트 내부 정적 데이터 사용하여 재생 확인완료

---
+++ src/components/MediaModal.jsx
@@ -0,0 +1,10 @@
+const MediaModal = () => {
+
+  return(
+    <div className="flex mt-36 w-5/6 h-80 border-2 border-color-white size-3/4">
+      <video src="" autoPlay />
+    </div>
+  );
+};
+
+export default MediaModal;
--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,8 +1,10 @@
+import MediaModal from "../components/MediaModal";
+
 const MainPage = () => {
 
   return(
     <div className="flex w-full min-h-screen bg-black items-center justify-center">
-      <span className="text-white text-2xl">로그인 후 메인화면 진입</span>
+      <MediaModal />
     </div>
   );
 };
