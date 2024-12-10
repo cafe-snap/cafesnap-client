80b1df92574ae6a4eb006fe086af3ea25fde6dca
Seongnam-si <hongdev26@gmail.com>
2024-12-10 15:47:27 +0900

    feat : 크롤링 대기시 UI인 로딩모달 생성

---
+++ src/components/LoadingModal.jsx
@@ -0,0 +1,10 @@
+const LoadingModal = () => {
+
+  return(
+    <div className="flex w-full min-h-screen justify-center items-center text-white text-5xl">
+      ...로딩중...
+    </div>
+  );
+};
+
+export default LoadingModal;
