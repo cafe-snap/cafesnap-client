bf223b780a6c6eba2f46792e6eb6acd619b47bee
Seongnam-si <hongdev26@gmail.com>
2024-12-18 15:18:58 +0900

    feat : 로딩모달 UI 수정 안내문구 앞 애니메이션 추가 애니메이션 관련 tailwind 설정 변경

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -2,7 +2,10 @@ const LoadingModal = () => {
 
   return(
     <div className="flex w-full min-h-screen justify-center items-center text-white text-5xl">
-      ...로딩중...
+      <div className="absolute w-4 h-4 rounded-full bg-green-400 animate-bigBounce" />
+      <div className="text-white text-lg font-light align-text-top ml-8 tracking-widest">
+        데이터를 불러오는 중입니다
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bigBounce: "bigBounce 500ms alternate infinite ease",
+      },
+      keyframes: {
+        bigBounce: {
+          "0%": {
+            top: "30px",
+            height: "5px",
+            borderRadius: "60px 60px 20px 20px",
+            transform: "scaleX(2)",
+          },
+          "35%": {
+            height: "16px",
+            borderRadius: "50%",
+            transform: "scaleX(1)",
+          },
+          "100%": {
+            top: "0",
+          },
+        },
+      },
+    }
   },
   plugins: [],
 };
