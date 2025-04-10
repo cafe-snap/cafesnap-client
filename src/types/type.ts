cdb3f68613e3cae4230a33f079e9c82ff25e2c22
Seongnam-si <hongdev26@gmail.com>
2025-04-10 13:52:38 +0900

    refactor : 공통 타입 관리를 위한 type.ts 파일 생성

---
+++ src/types/type.ts
@@ -0,0 +1,12 @@
+export type MediaItem = {
+  src: string;
+  type?: string;
+  postName?: string;
+  postLink?: string;
+}
+
+export interface CafeInfo {
+  cafeName: string;
+  cafeLink: string;
+  cafeLogo: string;
+}
