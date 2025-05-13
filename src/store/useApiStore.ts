607a4375f03e554b2aa90cd9a3dea7546c04589f
Seongnam-si <hongdev26@gmail.com>
2025-05-13 18:54:43 +0900

    chore : 타입스크립트 환경변수 사용관련 설정

--- src/store/useApiStore.ts
+++ src/store/useApiStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { MediaItem, CafeInfo } from "@/types/type";
 
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type RetryFunction = (...args: any[]) => Promise<any>;
 
